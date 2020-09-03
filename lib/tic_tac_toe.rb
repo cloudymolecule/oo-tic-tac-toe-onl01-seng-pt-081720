5f6d404d5fbb07d6eff06258ef3123210c45c669
Dario Carlino <baires82@gmail.com>
2020-09-03 02:50:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -91,15 +91,15 @@ class TicTacToe
   end
 
   def won?
-    win = []
+    win = ""
     WIN_COMBINATIONS.each do |comb|
       if comb[0] == @board[comb[0]] && comb[1] == @board[comb[1]] && comb[2] == @board[comb[2]]
-        win = [comb[0], comb[1], comb[2]]
+        win = "#{[comb[0]}, #{[comb[1]}, #{[comb[2]}"
       else
         false
       end
     end
-    if win == []
+    if win == ""
       false
     else
       win
